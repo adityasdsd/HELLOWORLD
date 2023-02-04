d8b64146875c4d4df4540a3a8ee084b4fc30e9ce
adity <adity@LAPTOP-Q7HOVIES>
2023-02-04 16:27:50 +0530

    add

--- HELLOWORLD/src/A1.java
+++ HELLOWORLD/src/A1.java
@@ -4,6 +4,12 @@ public class A1
 	public static void main(String args [] )
 	{
 		System.out.println("HELLO");
+		int arr [] = {12,12,12,12};
+		for(int x: arr)
+		{
+			System.out.println(x);
+		}
+		System.out.println("HELLO --->");
 	}
 
 }
